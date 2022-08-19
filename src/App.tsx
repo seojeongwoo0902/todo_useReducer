808096fc2c5bf2b1ec6f8324b5709a52ad4154ba
seojengwoo0902 <realteapots@vaultmicro.com>
2022-08-19 12:17:36 +0900

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <h2>clean</h2>
-    </div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>안녕하세요</TodoTemplate>
+    </>
+
   );
 }
 
